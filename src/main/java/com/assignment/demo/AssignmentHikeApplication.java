b74b9e9f7df275cbfbf9ff269aad25571fbf4897
omarfarukk <khan.omarfaruk@gmail.com>
2021-04-20 09:34:19 +0400

    Creating Spring Boot Project With Mongo and web with Actuator

---
+++ src/main/java/com/assignment/demo/AssignmentHikeApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentHikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentHikeApplication.class, args);
+	}
+
+}
